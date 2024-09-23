eb75cddb5be14a7d82cf60bf2588a6f11d1f8205
GitHarshalPatil <harshalpatil1847@gmail.com>
2024-09-23 14:54:11 +0530

    updated post req logging

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -20,7 +20,7 @@ function Users() {
         axios.post('http://localhost:4000/users', newUser)
             .then(response => {
                 setUsers([...users, response.data]); // Add new user to state
-                console.log(response.data);
+                console.log("this is my post data", response.data);
                 setNewUser({ name: '', email: '', age: '' }); // Reset form
             })
             .catch(error => console.log(error));
